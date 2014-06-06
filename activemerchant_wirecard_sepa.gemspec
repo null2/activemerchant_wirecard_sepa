ed64a13e66c349f7d312c1a98ce4c71e47729245
karsten gebbert <k@ioctl.it>
2014-06-06 15:22:46 +0200

    include I18n

--- activemerchant_wirecard_sepa.gemspec
+++ activemerchant_wirecard_sepa.gemspec
@@ -18,11 +18,12 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.6"
+  spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "nokogiri"
 
-  spec.add_runtime_dependency "activesupport"
-  spec.add_runtime_dependency "activemerchant"
+  spec.add_dependency "rails-i18n"
+  spec.add_dependency "activesupport"
+  spec.add_dependency "activemerchant"
 end
--- lib/active_merchant/billing/sepa_account.rb
+++ lib/active_merchant/billing/sepa_account.rb
@@ -13,15 +13,15 @@ module ActiveMerchant
         self.bic = options[:bic].strip if options[:bic]
       end
 
-    	def validate
-        errors.add :first_name, 'activerecord.errors.messages.empty'      if @first_name.blank?
-        errors.add :last_name,  'activerecord.errors.messages.empty'      if @last_name.blank?
+      def validate
+        errors.add :first_name, I18n.t('activerecord.errors.messages.empty')   if @first_name.blank?
+        errors.add :last_name,  I18n.t('activerecord.errors.messages.empty')   if @last_name.blank?
 
-        errors.add :iban, 'activerecord.errors.messages.empty'      if @iban.blank?
-        errors.add :bic,  'activerecord.errors.messages.empty'      if @bic.blank?
+        errors.add :iban,       I18n.t('activerecord.errors.messages.empty')   if @iban.blank?
+        errors.add :bic,        I18n.t('activerecord.errors.messages.empty')   if @bic.blank?
 
-        errors.add :iban, 'activerecord.errors.messages.invalid'    if @iban !~ /[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/
-        errors.add :bic, 'activerecord.errors.messages.invalid'    if @bic !~ /([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)/
+        errors.add :iban,       I18n.t('activerecord.errors.messages.invalid') if @iban !~ /[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/
+        errors.add :bic,        I18n.t('activerecord.errors.messages.invalid') if @bic !~ /([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)/
       end
 
       def to_s
